1351010ac9717df5721844b4ee55be50756089ee
Nivitha <nivithask@gmail.com>
2022-12-12 01:32:42 +0530

    Expense Feature Changes

---
+++ expense-tracker/src/components/expenses/expense-item/ExpenseItem.tsx
@@ -0,0 +1,18 @@
+import Card from '../../UI/card/Card';
+import ExpenseDate from '../expense-date/ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props: any) {
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
